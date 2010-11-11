5c9ca6eff21aeb33bbd73be808e7b1b2ffdc5ca7
santiago.videla <santiago.videla@localhost>
2010-11-11 20:08:57 +0000

    Ignore this. It just include the GPL header in all .cpp files.

--- src/RNAbackends/src/RNAForester.cpp
+++ src/RNAbackends/src/RNAForester.cpp
@@ -1,3 +1,28 @@
+/*
+ * File:   RNAForester.cpp
+ * Author: Santiago Videla <santiago.videla at gmail.com>
+ *
+ * Created on November 10, 2010, 4:26 PM
+ *
+ * Copyright (C) 2010  Santiago Videla, FuDePAN
+ *
+ * This file is part of vac-o
+ *
+ * vac-o is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * vac-o is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with vac-o.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "RNAForester.h"
 
 const FilePath RNAForester::IN = "forester.in";
