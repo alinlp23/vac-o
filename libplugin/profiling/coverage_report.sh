baf0eb2d39e81b9b4f1904f099ee54027741ca78
santiago.videla <santiago.videla@localhost>
2010-11-15 23:38:07 +0000

    Adds initial tests for libplugin package

---
+++ libplugin/profiling/coverage_report.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir gcov
+cd gcov
+cp ../../CMakeFiles/plugin.dir/src/*.gcno .
+cp ../../CMakeFiles/plugin.dir/src/*.gcda .
+
+gcov -o SequenceMutator.cpp.gcno ../../src/SequenceMutator.cpp
+gcov -o TripletsProductor.cpp.gcno ../../src/TripletsProductor.cpp
+
+cd ..
+geninfo --output-filename libplugin-coverage.info gcov
+genhtml -o coverage -t "vac-o libplugin pacakge" libplugin-coverage.info
+
+
+
